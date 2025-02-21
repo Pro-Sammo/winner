7a8b5243d395da8c4429a0bdfa5ae32887527bc3
Pro-sammo <ss.sammo2002@gmail.com>
2025-02-21 19:11:10 +0600

    fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,52 +1,34 @@
-"use client"
+"use client";
 
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { EffectFade, Navigation, Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
 
-// import required modules
-import {EffectFade, Navigation, Pagination ,Autoplay} from 'swiper/modules';
+import sliderImage01 from "../../../public/01.jpg";
+import sliderImage02 from "../../../public/02.jpg";
+import sliderImage03 from "../../../public/03.jpg";
 
-import Image from 'next/image';
-
-import sliderImage01 from "../../../public/01.jpg"
-import sliderImage02 from "../../../public/02.jpg"
-import sliderImage03 from "../../../public/03.jpg"
-
-
- const Hero =()=> {
+const Hero = () => {
   return (
-    <>
-      <Swiper
-        dir="ltr"
-        loop
-        effect={'fade'}
-        pagination={{
-          clickable: true,
-        }}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        modules={[Navigation, Pagination, Autoplay ,EffectFade]}
-        className="mySwiper w-full h-fit mt-[4.5rem]"
-      >
-        <SwiperSlide className='w-screen h-screen object-cover'><Image src={sliderImage01} className='object-cover'/></SwiperSlide>
+    <Swiper
+      loop
+      effect="fade"
+      fadeEffect={{ crossFade: true }}
+      centeredSlides={true}
+      autoplay={{ delay: 3000, disableOnInteraction: true }}
+      modules={[Navigation, Pagination, Autoplay, EffectFade]}
+      className="mySwiper w-full h-fit mt-[4.5rem]"
+    >
+      <SwiperSlide className='w-screen h-screen object-cover'><Image src={sliderImage01} className='object-cover'/></SwiperSlide>
         <SwiperSlide className='w-screen h-screen object-cover'><Image src={sliderImage02} className='object-cover'/></SwiperSlide>
         <SwiperSlide className='w-screen h-screen object-cover'><Image src={sliderImage03} className='object-cover'/></SwiperSlide>
-
-
-
-      </Swiper>
-    </>
+    </Swiper>
   );
-}
+};
 
-
-export default Hero
+export default Hero;
